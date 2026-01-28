38bb9741e52a34382dd6f899d2d226be40c10a44
leibniz-rs <matteo.cipollina@yahoo.it>
2026-01-28 11:31:38 +0100

    Update Limit.lean

--- Mathlib/Analysis/SpecialFunctions/Gamma/Binet/Limit.lean
+++ Mathlib/Analysis/SpecialFunctions/Gamma/Binet/Limit.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina, Jonathan Washburn
 -/
 
-import Mathlib.Analysis.SpecialFunctions.Gamma.Binet.Integral
+import Mathlib.Analysis.SpecialFunctions.Gamma.Binet.Kernel
 import Mathlib.Analysis.SpecialFunctions.Exp
 
 /-!
