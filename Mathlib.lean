439672a5a69c66ac6a3a637cb80727c03b7946ab
Matteo CIpollina <matteo.cipollina@yahoo.it>
2026-01-28 21:16:25 +0100

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1724,6 +1724,8 @@ public import Mathlib.Analysis.Complex.ValueDistribution.FirstMainTheorem
 public import Mathlib.Analysis.Complex.ValueDistribution.ProximityFunction
 public import Mathlib.Analysis.Complex.WeierstrassFactor.CanonicalProduct
 public import Mathlib.Analysis.Complex.WeierstrassFactor.Defs
+public import Mathlib.Analysis.Complex.WeierstrassFactor.Divisor.Convergence
+public import Mathlib.Analysis.Complex.WeierstrassFactor.Divisor.Index
 public import Mathlib.Analysis.Complex.WeierstrassFactor.Lemmas
 public import Mathlib.Analysis.ConstantSpeed
 public import Mathlib.Analysis.Convex.AmpleSet
