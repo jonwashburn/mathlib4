d8fb65131e92d7d9ed0b4c600f8e1c1c39f965da
Matteo CIpollina <matteo.cipollina@yahoo.it>
2026-01-28 16:43:48 +0100

    Update Bounds.lean

--- Mathlib/Analysis/SpecialFunctions/Gamma/Binet/Bounds.lean
+++ Mathlib/Analysis/SpecialFunctions/Gamma/Binet/Bounds.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Jonathan Washburn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Matteo Cipollina, Jonathan Washburn
+Authors: Matteo Cipollina
 -/
 
 import Mathlib.Analysis.SpecialFunctions.Gamma.Binet.Kernel
